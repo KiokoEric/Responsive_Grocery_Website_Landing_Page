a292bf3417e18e778f4bdeddf4fdaac2fd41f883
kiokoeric <kiokoerick040@gmail.com>
2024-01-19 15:57:12 +0300

    first commit

---
+++ JS/Grocery_Store_Website.js
@@ -0,0 +1,13 @@
+
+// NAVIGATION BAR
+
+let Bars = document.querySelector("#Bars");
+let Menu = document.querySelector(".Menu");
+
+Bars.addEventListener("click", function () {
+    this.classList.toggle("fa-xmark")
+    Menu.classList.toggle("Active")
+})
+
+// HEADER SCROLL CHANGE
+
